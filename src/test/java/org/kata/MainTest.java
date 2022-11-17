a06382215ed047785eb530afda67400f7264366b
Sven Hilsmann <sven.hilsmann.ext@uniper.energy>
2022-11-17 13:11:51 +0100

    create kata structure

---
+++ src/test/java/org/kata/MainTest.java
@@ -0,0 +1,14 @@
+package org.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MainTest {
+
+    @Test
+    void exampleTest() {
+        assertEquals(1, 1);
+    }
+
+}
